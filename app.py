93e917abb23f7ccd5ccf63dfca1b2d61d3eb4019
jopzz <68411637+jopzz@users.noreply.github.com>
2022-05-27 09:09:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, g, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+db.create_all()
+
+app.secret_key = 'somesecretkeythatonlyishouldknow'
+
+
+users = []
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
+users.append(User(id=4, username='Joyal', password='123456'))
+
+
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']]
+        if len(user) > 0:
+            g.user = user[0]
+        else:
+            g.user = None
+        
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route("/")
+def home():
+    
+    if not g.user:
+        return redirect(url_for('login'))
+
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
+
+@app.route("/logout")
+def logout():
+    session['user_id'] = None
+    g.user = None
+    return redirect(url_for('login'))
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/edit/<int:todo_id>", methods=["POST"])
+def edit(todo_id):
+    title = request.form.get("title")
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.title = title
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+   
+    app.run(debug=True)
